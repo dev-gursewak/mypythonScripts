b9222f297a9009d3d001299f7e9fd6b90a9d1a5f
Gursewak <gs2280431@gmail.com>
2023-06-21 21:16:29 +0530

    isbn number converted to list

--- isbn_10_validator.py
+++ isbn_10_validator.py
@@ -1,4 +1,12 @@
 def valid_ISBN10(isbn):
+    isbn_list = []
+    for i in isbn:
+        if i=='X':
+            isbn_list.append(10)
+        else:
+            isbn_list.append(i)
+    
+        
 
 
 
@@ -7,13 +15,21 @@ def valid_ISBN10(isbn):
 #position : 1 2 3 4 5 6 7 8 9 10
 #(1*1 + 1*2 + 1*3 + 2*4 + 2*5 + 2*6 + 3*7 + 3*8 + 3*9 + 9*10) % 11 = 0
 # X repersents 10
-valid_ISBN10('1112223339')
-valid_ISBN10('048665088X')
-valid_ISBN10('1293000000')
-valid_ISBN10('1234554321') #True
+print(valid_ISBN10('1112223339'))
+print(valid_ISBN10('048665088X'))
+print(valid_ISBN10('1293000000'))
+print(valid_ISBN10('1234554321')) #True
 valid_ISBN10('1234512345')
 valid_ISBN10('1293')
 valid_ISBN10('X123456788')
 valid_ISBN10('ABCDEFGHIJ')
 valid_ISBN10('XXXXXXXXXX')
 valid_ISBN10('123456789T') #False
+
+#check list
+#The character should be int expect X
+#X should be treated as digit which is 10.
+#The length should be 10 digits.
+#X is considerd as one digit.
+
+
